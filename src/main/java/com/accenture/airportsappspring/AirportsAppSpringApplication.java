4e7b861c9d203d215c8ad91aceb475fb63b22761
Andrea <andrea.scarselli@philips.com>
2022-01-20 21:38:45 +0100

    Initial commit

---
+++ src/main/java/com/accenture/airportsappspring/AirportsAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.airportsappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportsAppSpringApplication.class, args);
+    }
+
+}
