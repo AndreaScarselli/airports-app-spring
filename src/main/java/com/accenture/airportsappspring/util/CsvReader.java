f35d018ea291764764bd7690970c1e294635897a
Andrea <andrea.scarselli@philips.com>
2022-01-21 09:02:00 +0100

    Reading resources from classpath.

--- src/main/java/com/accenture/airportsappspring/util/CsvReader.java
+++ src/main/java/com/accenture/airportsappspring/util/CsvReader.java
@@ -3,6 +3,9 @@ package com.accenture.airportsappspring.util;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,13 +18,25 @@ import java.util.List;
 @Component
 public class CsvReader {
 
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public CsvReader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @SuppressWarnings("unchecked")
     public <T> List<T> readCsv(String fileName, Class<T> tClass) throws IOException {
         HeaderColumnNameMappingStrategy<T> headerColumnNameMappingStrategy = new HeaderColumnNameMappingStrategy<>();
         headerColumnNameMappingStrategy.setType(tClass);
-        Path path = Paths.get(fileName);
+
+        Resource resource = resourceLoader.getResource(fileName);
+
+        Path path = Paths.get(resource.getURI());
 
         Reader reader = Files.newBufferedReader(path);
+
+
         CsvToBean csvToBean = new CsvToBeanBuilder(reader)
                 .withType(tClass)
                 .withMappingStrategy(headerColumnNameMappingStrategy)
