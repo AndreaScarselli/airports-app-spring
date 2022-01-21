6ca7c6ab488eb6c473c99660d9b908b82db0c3de
Andrea <andrea.scarselli@philips.com>
2022-01-21 16:44:27 +0100

    Initialize DB logic moved from CommandLineRunner to CsvReader, AirportsAppCommandLineRunner uses now a "prod" profile, will not be used in testing.

--- src/main/java/com/accenture/airportsappspring/AirportsAppCommandLineRunner.java
+++ src/main/java/com/accenture/airportsappspring/AirportsAppCommandLineRunner.java
@@ -3,78 +3,32 @@ package com.accenture.airportsappspring;
 import com.accenture.airportsappspring.model.Airport;
 import com.accenture.airportsappspring.model.Country;
 import com.accenture.airportsappspring.model.Runway;
-import com.accenture.airportsappspring.repository.AirportRepository;
-import com.accenture.airportsappspring.repository.CountryRepository;
-import com.accenture.airportsappspring.repository.RunwayRepository;
 import com.accenture.airportsappspring.service.CountryWithMostAirportsRetriever;
 import com.accenture.airportsappspring.service.RunwaysRetriever;
-import com.accenture.airportsappspring.util.CsvReader;
 import com.accenture.airportsappspring.util.CountryWithNumberOfAirports;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 @Component
+@Profile("prod")
 public class AirportsAppCommandLineRunner implements CommandLineRunner {
 
-    @Value("${csv-path.countries}")
-    private String countriesCsvPath;
-
-    @Value("${csv-path.runways}")
-    private String runwaysCsvPath;
-
-    @Value("${csv-path.airports}")
-    private String airportsCsvPath;
-
-    private final CsvReader csvReader;
-
-    private final AirportRepository airportRepository;
-    private final CountryRepository countryRepository;
-    private final RunwayRepository runwayRepository;
     private final CountryWithMostAirportsRetriever countryWithMostAirportsRetriever;
     private final RunwaysRetriever runwaysRetriever;
 
-    private static final Logger LOG = LoggerFactory.getLogger(AirportsAppCommandLineRunner.class);
-
-    public AirportsAppCommandLineRunner(CsvReader csvReader,
-                                        AirportRepository airportRepository,
-                                        CountryRepository countryRepository,
-                                        RunwayRepository runwayRepository,
-                                        RunwaysRetriever runwaysRetriever,
-                                        CountryWithMostAirportsRetriever countryWithMostAirportsRetriever){
-        this.csvReader = csvReader;
-        this.airportRepository = airportRepository;
-        this.countryRepository = countryRepository;
-        this.runwayRepository = runwayRepository;
+    public AirportsAppCommandLineRunner(RunwaysRetriever runwaysRetriever, CountryWithMostAirportsRetriever countryWithMostAirportsRetriever) {
         this.runwaysRetriever = runwaysRetriever;
         this.countryWithMostAirportsRetriever = countryWithMostAirportsRetriever;
     }
 
-    @PostConstruct
-    private void initializeDataBase() throws IOException {
-        LOG.info("Reading CSVs.");
-        List<Airport> airports = csvReader.readCsv(airportsCsvPath, Airport.class);
-        List<Runway> runways = csvReader.readCsv(runwaysCsvPath, Runway.class);
-        List<Country> countries = csvReader.readCsv(countriesCsvPath, Country.class);
-
-        LOG.info("Saving entities in the DB.");
-        airportRepository.saveAll(airports);
-        runwayRepository.saveAll(runways);
-        countryRepository.saveAll(countries);
-        LOG.info("Saving Entities - Done!");
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        while(true) {
+        while (true) {
             System.out.println("Enter country or country code: \n");
             Scanner scanner = new Scanner(System.in);
             String country = scanner.nextLine();
--- src/main/java/com/accenture/airportsappspring/util/CsvReader.java
+++ src/main/java/com/accenture/airportsappspring/util/CsvReader.java
@@ -1,13 +1,22 @@
 package com.accenture.airportsappspring.util;
 
+import com.accenture.airportsappspring.model.Airport;
+import com.accenture.airportsappspring.model.Country;
+import com.accenture.airportsappspring.model.Runway;
+import com.accenture.airportsappspring.repository.AirportRepository;
+import com.accenture.airportsappspring.repository.CountryRepository;
+import com.accenture.airportsappspring.repository.RunwayRepository;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -18,11 +27,44 @@ import java.util.List;
 @Component
 public class CsvReader {
 
-    private final ResourceLoader resourceLoader;
+    @Value("${csv-path.countries}")
+    private String countriesCsvPath;
 
-    @Autowired
-    public CsvReader(ResourceLoader resourceLoader) {
+    @Value("${csv-path.runways}")
+    private String runwaysCsvPath;
+
+    @Value("${csv-path.airports}")
+    private String airportsCsvPath;
+
+    private final ResourceLoader resourceLoader;
+    private final AirportRepository airportRepository;
+    private final CountryRepository countryRepository;
+    private final RunwayRepository runwayRepository;
+
+    private static final Logger LOG = LoggerFactory.getLogger(CsvReader.class);
+
+    public CsvReader(ResourceLoader resourceLoader,
+                     AirportRepository airportRepository,
+                     CountryRepository countryRepository,
+                     RunwayRepository runwayRepository) {
         this.resourceLoader = resourceLoader;
+        this.airportRepository = airportRepository;
+        this.countryRepository = countryRepository;
+        this.runwayRepository = runwayRepository;
+    }
+
+    @PostConstruct
+    private void initializeDataBase() throws IOException {
+        LOG.info("Reading CSVs.");
+        List<Airport> airports = readCsv(airportsCsvPath, Airport.class);
+        List<Runway> runways = readCsv(runwaysCsvPath, Runway.class);
+        List<Country> countries = readCsv(countriesCsvPath, Country.class);
+
+        LOG.info("Saving entities in the DB.");
+        airportRepository.saveAll(airports);
+        runwayRepository.saveAll(runways);
+        countryRepository.saveAll(countries);
+        LOG.info("Saving Entities - Done!");
     }
 
     @SuppressWarnings("unchecked")
@@ -31,11 +73,8 @@ public class CsvReader {
         headerColumnNameMappingStrategy.setType(tClass);
 
         Resource resource = resourceLoader.getResource(fileName);
-
         Path path = Paths.get(resource.getURI());
-
         Reader reader = Files.newBufferedReader(path);
-
 
         CsvToBean csvToBean = new CsvToBeanBuilder(reader)
                 .withType(tClass)
