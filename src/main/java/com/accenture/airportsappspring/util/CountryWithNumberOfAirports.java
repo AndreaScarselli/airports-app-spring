cb796329899c3bb7dda7d67d74497c5ccba7efaa
Andrea <andrea.scarselli@philips.com>
2022-01-23 19:40:26 +0100

    Change toString in CountryWithNumberOfAirports

--- src/main/java/com/accenture/airportsappspring/util/CountryWithNumberOfAirports.java
+++ src/main/java/com/accenture/airportsappspring/util/CountryWithNumberOfAirports.java
@@ -3,12 +3,16 @@ package com.accenture.airportsappspring.util;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@Getter @Setter @AllArgsConstructor @ToString
+@Getter @Setter @AllArgsConstructor
 public class CountryWithNumberOfAirports {
 
     private String countryName;
     private String isoCountry;
     private Long numberOfAirports;
+
+    @Override
+    public String toString() {
+        return "Country=" + countryName + " has " + numberOfAirports + " airports.";
+    }
 }
