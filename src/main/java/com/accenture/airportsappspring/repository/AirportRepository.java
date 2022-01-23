b4610837d9da9fc015d62e2bf80ec9e1621a857c
Andrea <andrea.scarselli@philips.com>
2022-01-23 11:21:55 +0100

    Moved findCountriesWithMostAirports from AirportRepository to CountryRepository

--- src/main/java/com/accenture/airportsappspring/repository/AirportRepository.java
+++ src/main/java/com/accenture/airportsappspring/repository/AirportRepository.java
@@ -4,20 +4,12 @@
 package com.accenture.airportsappspring.repository;
 
 import com.accenture.airportsappspring.model.Airport;
-import com.accenture.airportsappspring.util.CountryWithNumberOfAirports;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface AirportRepository extends CrudRepository<Airport,String> {
 
-    public List<Airport> findAllByIsoCountryIgnoreCase(String country);
+    List<Airport> findAllByIsoCountryIgnoreCase(String country);
 
-    @Query("select new com.accenture.airportsappspring.util.CountryWithNumberOfAirports(c.name, a.isoCountry, count(a.isoCountry)) \n" +
-            "from Airport a, Country c\n" +
-            "where a.isoCountry = c.code\n" +
-            "group by a.isoCountry, c.name\n" +
-            "order by count(a.isoCountry) desc")
-    public List<CountryWithNumberOfAirports> findCountriesWithMostAirports();
 }
--- src/main/java/com/accenture/airportsappspring/repository/CountryRepository.java
+++ src/main/java/com/accenture/airportsappspring/repository/CountryRepository.java
@@ -4,13 +4,22 @@
 package com.accenture.airportsappspring.repository;
 
 import com.accenture.airportsappspring.model.Country;
+import com.accenture.airportsappspring.util.CountryWithNumberOfAirports;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface CountryRepository extends CrudRepository<Country,String> {
 
-    public List<Country> findByNameContainingIgnoreCase(String name);
+    List<Country> findByNameContainingIgnoreCase(String name);
 
-    public Country findByCodeIgnoreCase(String code);
+    Country findByCodeIgnoreCase(String code);
+
+    @Query("select new com.accenture.airportsappspring.util.CountryWithNumberOfAirports(c.name, a.isoCountry, count(a.isoCountry)) \n" +
+            "from Airport a, Country c\n" +
+            "where a.isoCountry = c.code\n" +
+            "group by a.isoCountry, c.name\n" +
+            "order by count(a.isoCountry) desc")
+    List<CountryWithNumberOfAirports> findCountriesWithMostAirports();
 }
